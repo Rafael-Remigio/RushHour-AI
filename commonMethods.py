c04ae95fa8b7f1267ba738e7491c871f36e6decf
rafael <rafael.lixa@ua.pt>
2022-12-05 15:46:41 +0000

    heuristics added

--- commonMethods.py
+++ commonMethods.py
@@ -5,7 +5,34 @@ import copy
 
 import time
 
+
+
+import heapq
+
+class MyHeap(object):
+    def __init__(self, initial=None, key=lambda x:x):
+        self.key = key
+        self.index = 0
+        if initial:
+            self._data = [(key(item), i, item) for i, item in enumerate(initial)]
+            self.index = len(self._data)
+            heapq.heapify(self._data)
+        else:
+            self._data = []
+
+    def push(self, item):
+        heapq.heappush(self._data, (self.key(item), self.index, item))
+        self.index += 1
+
+    def pop(self):
+        return heapq.heappop(self._data)[2]
+
+
+
 # Methods
+
+
+
 
 
 def stringToGrid(mapString):
@@ -28,6 +55,31 @@ def stringToGrid(mapString):
             line = []
 
     return (grid,pieceSet,"")
+
+
+    
+
+def stringToGridA(mapString):
+    """Initializes Map Tuple from a MapString Representation and initializes cost at 0"""
+    mapString = mapString.split(" ")[1]
+    grid_size = int(math.sqrt(len(mapString)))
+    pieceSet = []
+    grid = []
+    line = []
+    for i, pos in enumerate(mapString):
+
+        # Creates an Array with all the diferent Pieces
+        if (pos not in {"o","x"} ) and (pos not in pieceSet):
+            pieceSet.append(pos)
+        
+        #
+        line.append(pos)
+        if (i + 1) % grid_size == 0:
+            grid.append(line)
+            line = []
+
+    return (grid,pieceSet,"",0)
+
 
 def coordinates(grid):
     """Representation of ocupied map positions through tuples x,y,carValue."""
@@ -171,6 +223,83 @@ def possibleMoves(map):
     return possibleStates
 
 
+def customHeuristics(grid,_coordinates):
+
+    grid_size = len(grid)
+
+    cords = piece_coordinates("A",_coordinates)[1]
+
+
+
+    return grid_size - cords[0]
+
+
+def possibleMovesAStart(map):
+    """List of possible foward states from a given Map Tuple"""
+    possibleStates = []
+
+    _coordinates = coordinates(grid=map[0])
+
+    for car in map[1]:
+        carCords = piece_coordinates(car,_coordinates)
+
+        if carCords[0][1] == carCords[1][1]:
+
+            if canMove(map[0],car,(-1,0),carCords):
+
+                grid2 = copy.deepcopy(map[0])
+                move(grid2,car,carCords,(-1,0))
+
+                map2 = (grid2,map[1],map[2] + car + "a",customHeuristics(grid2,_coordinates))  # Creates a copy of the current Map Tuple and Updates the current Solution
+
+
+                
+                possibleStates.append(map2) # Appends it to the possible states
+
+
+            if canMove(map[0],car,(1,0),carCords):
+
+
+
+                grid2 = copy.deepcopy(map[0])
+                move(grid2,car,carCords,(1,0))
+
+                map2 = (grid2,map[1],map[2] + car + "d",customHeuristics(grid2,_coordinates))  # Creates a copy of the current Map Tuple and Updates the current Solution
+
+                
+                possibleStates.append(map2) # Appends it to the possible states
+
+                
+        else:
+            if canMove(map[0],car,(0,-1),carCords):
+
+
+                grid2 = copy.deepcopy(map[0])
+                move(grid2,car,carCords,(0,-1))
+
+
+                map2 = (grid2,map[1],map[2] + car + "w",customHeuristics(grid2,_coordinates))  # Creates a copy of the current Map Tuple and Updates the current Solution
+
+                
+                possibleStates.append(map2) # Appends it to the possible states
+
+
+            if canMove(map[0],car,(0,1),carCords):
+
+
+                grid2 = copy.deepcopy(map[0])
+                move(grid2,car,carCords,(0,1))
+
+                map2 = (grid2,map[1],map[2] + car + "s",customHeuristics(grid2,_coordinates))  # Creates a copy of the current Map Tuple and Updates the current Solution
+
+                
+                possibleStates.append(map2) # Appends it to the possible states
+
+
+
+    return possibleStates
+
+
 
 def breathsearch(startState):
     mapa = stringToGrid(startState)
@@ -196,16 +325,53 @@ def breathsearch(startState):
 
 
 
+def AStar(startState):
+    mapa = stringToGridA(startState) # returns a tuple with a cost starting at 0
+    open_nodes = MyHeap([mapa],key=lambda x: x[3]) # Uses  a heap to keep the nodes sorted
+    visitedNodes = set()
+
+
+    while open_nodes != []:
+
+        node = open_nodes.pop()
+
+        if test_win(node[0]):
+            solution = node
+            return solution[2]
+
+
+        for a in possibleMovesAStart(node):
+            if not visitedNodes.__contains__(str(a[0])):
+                open_nodes.push(a) # Pushes on to the heap. keeps the nodes sorted
+                visitedNodes.add(str(a[0]))
+
+
+
+    return None
+
+
+
 
 if __name__ == "__main__":
     
     file1 = open('levels.txt', 'r')
     Lines = file1.readlines()
     j =1
+    startTime = 0
+    endTime = 0
+    searchType = "A*"
     for i in Lines:
 
-        startTime = time.time()
-        print(breathsearch(i))
-        endTime = time.time()
-        print("lever nº " + str(j) + " time is " + str(endTime - startTime) + " seconds")
+        if searchType == "breathSearch":
+            startTime = time.time()
+            print(breathsearch(i))
+            endTime = time.time()
+
+        if searchType == "A*":
+            startTime = time.time()
+            print(AStar(i))
+            endTime = time.time()
+    
+    
+        print("level nº " + str(j) + " time is " + str(endTime - startTime) + " seconds")
         j+=1
